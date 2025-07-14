dd25fb809da19e325c15d29c878f0e4364def632
Pinaki Jha <pinakijah@gmail.com>
2025-07-15 03:11:50 +0530

    testing out youtube embeds

--- routes/workshopPage.js
+++ routes/workshopPage.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const WorkshopsPage = require('../models/workshopPage');
 
+
 router.get('/', async (req, res) => {
   try {
     const page = await WorkshopsPage.findOne(); // Assuming one document
     //console.log(page)
+    
     res.json(page);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch workshops data.' });
--- routes/workshopSection.js
+++ routes/workshopSection.js
@@ -5,7 +5,7 @@ const WorkshopsSection = require('../models/workshopSection');
 // GET workshops data
 router.get('/', async (req, res) => {
   try {
-    const workshops = await WorkshopsSection.findOne(); // assuming only one document
+    const workshops = await WorkshopsSection.findOne(); 
     res.json(workshops);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch workshops section' });
